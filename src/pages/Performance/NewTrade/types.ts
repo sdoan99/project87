79fad6b8876cf0ea889bc3e31202aba1b8613c25
calliechatai <callie.chatai@gmail.com>
2025-04-20 11:10:23 -0600

    Removed NewTrade/types.ts

--- src/pages/Performance/NewTrade/types.ts
+++
@@ -1,7 +0,0 @@
-// TradeActionType and TradeAction have been centralized in src/types/trade.ts
-// import { TradeAction, TradeActionType } from 'src/types/trade';
-
-export interface MarketSelectProps {
-  value: string;
-  onChange: (value: string) => void;
-}
